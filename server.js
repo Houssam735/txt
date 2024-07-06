ce2aea3502ba20e70d7c5dabe6bde47336bca388
Houssam735 <57289017+Houssam735@users.noreply.github.com>
2024-07-06 20:32:53 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { PythonShell } = require('python-shell');
 require('dotenv').config();
-
+const keep_alive = require('./keep_alive.js')
 PythonShell.run('main.py', null, function (err, results) {
   if (err) throw err;
   console.log('Results:', results);
