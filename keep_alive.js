4abef716e2678c655e82dd2bba4327873a318da6
Houssam735 <57289017+Houssam735@users.noreply.github.com>
2024-07-06 20:31:06 +0000

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 var http = require('http')
 
-http.createServer(function (req, res) 
+http.createServer(function (req, res) {
   res.write("I'm alive");
   res.end();
- ).listen(8080);
+}).listen(8080);
